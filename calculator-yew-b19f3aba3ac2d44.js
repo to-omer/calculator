133f1c87a4be3e558e95914a5893947d1ebc5f70
to-omer <to-omer@users.noreply.github.com>
2023-06-15 16:07:07 +0000

    deploy: d1881298388925844babcaf903d4343a420b5d0c

--- calculator-yew-b19f3aba3ac2d44.js
+++ calculator-yew-b19f3aba3ac2d44.js
@@ -684,7 +684,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('calculator-yew-f461af58cd967170_bg.wasm', import.meta.url);
+        input = new URL('calculator-yew-b19f3aba3ac2d44_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
